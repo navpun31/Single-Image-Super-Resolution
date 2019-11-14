ef56d2a03eb6336dbfe2b2d3b5b73901f5811ec5
Naveen Punjabi <navpun31@gmail.com>
2019-11-15 01:39:59 +0530

    Perception Distortion Tradeoff

--- evaluate.py
+++ evaluate.py
@@ -1,4 +1,4 @@
-import sewar
+import sewar, math
 
 def psnr(image_sr,image_hr):
 	return sewar.psnr(image_hr,image_sr)
@@ -6,10 +6,15 @@ def psnr(image_sr,image_hr):
 def mse(image_sr,image_hr):
 	return sewar.mse(image_hr,image_sr)
 
+def rmse(image_sr,image_hr):
+	return math.sqrt( mse(image_sr,image_hr) )
+
 def ssim(image_sr,image_hr):
 	return sewar.ssim(image_hr,image_sr)[0]
 
 def uqi(image_sr,image_hr):
 	return sewar.uqi(image_hr,image_sr)
 
-
+def d_lambda(image_sr,image_bi):
+    # Params: Bicubic and Super-Resolved Images     
+    return sewar.no_ref.d_lambda(image_bi,image_sr)
