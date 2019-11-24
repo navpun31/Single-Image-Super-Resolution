bee5badedcd40c1a0a0d798991571d6e93258ee7
Naveen Punjabi <navpun31@gmail.com>
2019-11-24 16:38:35 +0530

    Added Plots to PD Tradeoff

--- evaluate.py
+++ evaluate.py
@@ -1,4 +1,6 @@
 import sewar, math
+from libsvm import svmutil
+from brisque import *
 
 def psnr(image_sr,image_hr):
 	return sewar.psnr(image_hr,image_sr)
@@ -15,6 +17,10 @@ def ssim(image_sr,image_hr):
 def uqi(image_sr,image_hr):
 	return sewar.uqi(image_hr,image_sr)
 
-def d_lambda(image_sr,image_bi):
+def d_lambda(image_sr,image_bi,path=None):
     # Params: Bicubic and Super-Resolved Images     
     return sewar.no_ref.d_lambda(image_bi,image_sr)
+
+def brisque(image_sr,image_bi,path=None):
+    bris = BRISQUE()
+    return bris.get_score(path)
